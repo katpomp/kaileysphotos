e6609832d1f8e58e75c5fdd1328ea397fc008aee
katpomp <katpompp@gmail.com>
2022-03-24 10:30:56 -0400

    yes

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def mommy():
     return render_template('index.html')
+
+
+@app.route('/football')
+def football():
+    return render_template('football.html')
+
+
+@app.route('/aboutme')
+def aboutme():
+    return render_template('aboutme.html')
 
 
 if __name__ == "__main__":
